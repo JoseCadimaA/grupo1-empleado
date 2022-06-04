e482b72fe47b56749a3ebd2146fb334e8142561d
José Manuel Cadima <jose75684847@gmail.com>
2022-06-04 05:49:00 -0400

    Domain Tests

---
+++ EmpleadoTest/Domain/EmpleadoFactory_Test.cs
@@ -0,0 +1,30 @@
+﻿using Empleados.Domain.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmpleadoTest.Domain
+{
+    public class EmpleadoFactory_Test
+    {
+        [Fact]
+        public void Create_Correctly()
+        {
+
+            var nombreCompletoTest = "Pepe Cadima";
+            var fechaNacimientoTest = new DateTime(1996, 10, 04);
+            var ciTest = "8137916";
+
+            var factory = new EmpleadoFactory();
+            var objEmpleado = factory.Create(nombreCompletoTest, fechaNacimientoTest, ciTest);
+
+            Assert.NotNull(objEmpleado);
+            Assert.Equal(nombreCompletoTest, objEmpleado.NombreCompleto);
+            Assert.Equal(fechaNacimientoTest, (DateTime)objEmpleado.FechaNacimiento);
+            Assert.Equal(ciTest, objEmpleado.CI);
+        }
+    }
+}
---
+++ EmpleadoTest/Domain/HistoricoNavegacionFactory_Test.cs
@@ -0,0 +1,34 @@
+﻿using Empleados.Domain.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmpleadoTest.Domain
+{
+    public class HistoricoNavegacionFactory_Test
+    {
+        [Fact]
+        public void IsData_Valid()
+        {
+            var empleadoIDTest = Guid.NewGuid().ToString();
+            var horasRealizadaTest = 2;
+            var millasTest = 1000;
+            var origenTest = "SC";
+            var destinoTest = "LP";
+
+            var factory = new HistoricoNavegacionFactory();
+            var objHistorico = factory.Create(empleadoIDTest, 2, 1000, "SC", "LP" );
+
+            Assert.NotNull(objHistorico);
+            Assert.Equal(empleadoIDTest, objHistorico.EmpleadoID);
+            Assert.Equal(horasRealizadaTest, objHistorico.HorasRealizada);
+            Assert.Equal(millasTest, objHistorico.Millas);
+            Assert.Equal(origenTest, objHistorico.Origen);
+            Assert.Equal(destinoTest, objHistorico.Destino);
+        }
+
+    }
+}
