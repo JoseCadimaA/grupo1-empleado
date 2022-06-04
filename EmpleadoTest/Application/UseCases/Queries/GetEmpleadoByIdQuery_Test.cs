10a64d13d4e9df75116cfbf897ba72313a37f62b
José Manuel Cadima <jose75684847@gmail.com>
2022-06-04 07:25:47 -0400

    GetEmpleadoById

---
+++ EmpleadoTest/Application/UseCases/Queries/GetEmpleadoByIdQuery_Test.cs
@@ -0,0 +1,31 @@
+﻿using Empleados.Application.UseCases.Queries.Empleados.GetEmpleadoById;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmpleadoTest.Application.UseCases.Queries
+{
+    public class GetEmpleadoByIdQuery_Test
+    {
+        [Fact]
+        public void IsRequest_Valid()
+        {
+            var id_Test = Guid.NewGuid() ; 
+
+            var command = new GetEmpleadoByIdQuery(id_Test);
+
+            Assert.Equal(id_Test, command.Id);
+        }
+
+
+        [Fact]
+        public void TestConstructor_IsPrivate()
+        {
+            var command = (GetEmpleadoByIdQuery)Activator.CreateInstance(typeof(GetEmpleadoByIdQuery), true);
+            Assert.Equal(Guid.Empty,  command.Id);
+        }
+    }
+}
---
+++ EmpleadoTest/Application/UseCases/Queries/GetHistoricoByIdEmpleadoQuery_Test.cs
@@ -0,0 +1,34 @@
+﻿using Empleados.Application.UseCases.Queries.Empleados.GetHistoricoByIdEmpleado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmpleadoTest.Application.UseCases.Queries
+{
+    public class GetHistoricoByIdEmpleadoQuery_Test
+    {
+        [Fact]
+        public void IsRequest_Valid()
+        {
+            var id_Test = Guid.NewGuid();
+            var empleadoId_Test = "60cfbed6-f6b2-42aa-a024-91e240555e1e";
+
+            var command = new GetHistoricoByIdEmpleadoQuery(id_Test);
+            var command2 = new GetHistoricoByIdEmpleadoQuery(id_Test, empleadoId_Test);
+
+            Assert.Equal(id_Test, command.Id);
+            Assert.Equal(empleadoId_Test, command2.EmpleadoID);
+        }
+
+
+        [Fact]
+        public void TestConstructor_IsPrivate()
+        {
+            var command = (GetHistoricoByIdEmpleadoQuery)Activator.CreateInstance(typeof(GetHistoricoByIdEmpleadoQuery), true);
+            Assert.Equal(Guid.Empty, command.Id);
+        }
+    }
+}
