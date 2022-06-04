933ef39dcb1216d77fcc962e26ba27d7d486079d
José Manuel Cadima <jose75684847@gmail.com>
2022-06-04 00:10:22 -0400

    DTO Test

---
+++ EmpleadoTest/Application/Dto/EmpleadoDto_Test.cs
@@ -0,0 +1,48 @@
+﻿using Empleados.Application.Dto.Empleados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmpleadoTest.Application.Dto
+{
+    public class EmpleadoDto_Test
+    {
+
+        [Fact]
+        public void IsData_Valid()
+        {
+            /*
+                public Guid Id { get; set; }
+                public string NombreCompleto { get; set; }
+                public DateTime FechaNacimiento { get; set; }
+                public string CI { get; set; }
+            */
+            var idTest = Guid.NewGuid();
+            var nombreCompletoTest = "José Cadima";
+            var fechaNacimientoTest = new DateTime(1996, 10, 04);
+            var ciTest = "8137916";
+
+            var objEmpleadoDto = new EmpleadoDto();
+
+            Assert.Equal(Guid.Empty, objEmpleadoDto.Id);
+            Assert.Null(objEmpleadoDto.NombreCompleto);
+            Assert.Equal(DateTime.MinValue, objEmpleadoDto.FechaNacimiento);
+            Assert.Null(objEmpleadoDto.CI);
+
+            objEmpleadoDto.Id = idTest;
+            objEmpleadoDto.NombreCompleto = nombreCompletoTest;
+            objEmpleadoDto.FechaNacimiento = fechaNacimientoTest;
+            objEmpleadoDto.CI = ciTest;
+
+            Assert.Equal(idTest, objEmpleadoDto.Id);
+            Assert.Equal(nombreCompletoTest, objEmpleadoDto.NombreCompleto);
+            Assert.Equal(fechaNacimientoTest, objEmpleadoDto.FechaNacimiento);
+            Assert.Equal(ciTest, objEmpleadoDto.CI);
+
+   
+        }
+    }
+}
---
+++ EmpleadoTest/Application/Dto/HistoricoNavegacionDto_Test.cs
@@ -0,0 +1,60 @@
+﻿using Empleado.Application.Dto.Historicos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmpleadoTest.Application.Dto
+{
+    public class HistoricoNavegacionDto_Test
+    {
+        [Fact]
+        public void IsData_Valid()
+        {
+            /*
+             * public Guid Id { get; set; }
+                public Guid EmpleadoID { get; set; }
+                public int HorasRealizada { get; set; }
+                public int Millas { get; set; }
+                public string Origen { get; set; }
+                public string Destino { get; set; }
+             */
+
+            var idTest = Guid.NewGuid();
+            var empleadoIDTest = Guid.NewGuid();
+            var horasRealizadaTest=  2;
+            var millasTest = 1000;
+            var origenTest = "SC";
+            var destinoTest = "LP";
+
+            var objHistorico = new HistoricoNavegacionDto();
+
+            Assert.Equal(Guid.Empty, objHistorico.Id);
+            Assert.Equal(Guid.Empty, objHistorico.EmpleadoID);
+            Assert.Equal(0, objHistorico.HorasRealizada);
+            Assert.Equal(0, objHistorico.Millas);
+            Assert.Null(objHistorico.Origen);
+            Assert.Null(objHistorico.Destino);
+
+            objHistorico = new HistoricoNavegacionDto(idTest, empleadoIDTest, horasRealizadaTest, millasTest, origenTest, destinoTest);
+
+            //objHistorico.Id = idTest;
+            //objHistorico.EmpleadoID = empleadoIDTest;
+            //objHistorico.HorasRealizada = horasRealizadaTest;
+            //objHistorico.Millas = millasTest;
+            //objHistorico.Origen = origenTest;
+            //objHistorico.Destino = destinoTest;
+
+            Assert.Equal(idTest, objHistorico.Id);
+            Assert.Equal(empleadoIDTest, objHistorico.EmpleadoID);
+            Assert.Equal(horasRealizadaTest, objHistorico.HorasRealizada);
+            Assert.Equal(millasTest, objHistorico.Millas);
+            Assert.Equal(origenTest, objHistorico.Origen);
+            Assert.Equal(destinoTest, objHistorico.Destino);
+
+
+        }
+    }
+}
